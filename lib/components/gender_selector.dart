9e2f3660facdf1bea2e982b3ab332d3c4351c84f
Marianvsf <marianvsf@gmail.com>
2025-10-19 20:35:18 -0400

    gender selector

---
+++ lib/components/gender_selector.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_colors.dart';
+import 'package:imc_calculator/core/text_styles.dart';
+
+class GenderSelector extends StatefulWidget {
+  const GenderSelector({super.key});
+
+  @override
+  State<GenderSelector> createState() => _GenderSelectorState();
+}
+
+class _GenderSelectorState extends State<GenderSelector> {
+  String? selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // Male
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedGender = "Male";
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 16, right: 8, bottom: 16, left: 16),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: selectedGender == "Male"
+                        ? AppColors.backgroundComponentSelected
+                        : AppColors.backgroundComponent,
+                    borderRadius: BorderRadius.circular(16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/male.png", height: 100),
+                      SizedBox(height: 8),
+                      Text("Male".toUpperCase(), style: TextStyles.bodyText)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        // Female
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedGender = "Female";
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 16, right: 16, bottom: 16, left: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: selectedGender == "Female"
+                        ? AppColors.backgroundComponentSelected
+                        : AppColors.backgroundComponent,
+                    borderRadius: BorderRadius.circular(16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/female.png", height: 100),
+                      SizedBox(height: 8),
+                      Text("Female".toUpperCase(), style: TextStyles.bodyText)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,12 +1,12 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primary = Color(0xFF00B8E6);
+  static const Color primary = Color(0xFF4851C4);
   static const Color secondary = Color(0xFF00BFFF);
   static const Color accent = Color(0xFF2B00FF);
 
   // Backgrounds
   static const Color background = Color(0xFF020741);
   static const Color backgroundComponent = Color(0xFF39409E);
-  static const Color backgroundComponentSelected = Color(0xFF6E71A3);
+  static const Color backgroundComponentSelected = Color(0xFF585CB5);
 }
